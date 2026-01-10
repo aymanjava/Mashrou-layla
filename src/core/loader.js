f6eb4a03da8df615a9f4a4a06cefd95355402944
aymanjava <kkayman200@gmail.com>
2026-01-10 20:58:25 +0000

    Update loader.js

--- src/core/loader.js
+++ src/core/loader.js
@@ -1,21 +1,24 @@
 const fs = require("fs");
 const path = require("path");
 
-const commands = [];
-
-function loadCommands() {
-  const commandFiles = fs.readdirSync(path.join(__dirname, "../commands")).filter(file => file.endsWith(".js"));
+module.exports.loadCommands = (client) => {
+  const commandFiles = fs.readdirSync(path.join(__dirname, "../commands"))
+    .filter(file => file.endsWith(".js"));
 
   for (const file of commandFiles) {
-    try {
-      const command = require(`../commands/${file}`);
-      commands.push(command);
-    } catch (err) {
-      console.error(`خطأ في تحميل الأمر ${file}:`, err.message);
-    }
+    const command = require(`../commands/${file}`);
+    client.commands.set(command.config.name, command);
   }
+  console.log(`[✅] تم تحميل ${commandFiles.length} أوامر`);
+};
 
-  return commands;
-}
+module.exports.loadEvents = (client) => {
+  const eventFiles = fs.readdirSync(path.join(__dirname, "../events"))
+    .filter(file => file.endsWith(".js"));
 
-module.exports = { loadCommands };
+  for (const file of eventFiles) {
+    const event = require(`../events/${file}`);
+    client.on(event.name, (...args) => event.run(client, ...args));
+  }
+  console.log(`[✅] تم تحميل ${eventFiles.length} أحداث`);
+};
