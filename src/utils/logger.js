7d1c4acafc1a981035a147b0a9af6685815e31e4
aymanjava <kkayman200@gmail.com>
2026-01-11 09:55:49 +0000

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,18 +1,10 @@
-const colors = {
-  reset: "\x1b[0m",
-  green: "\x1b[32m",
-  red: "\x1b[31m",
-  yellow: "\x1b[33m"
-};
+const fs = require('fs');
+const path = require('path');
 
-module.exports = {
-  info(msg) {
-    console.log(colors.green + "[INFO]" + colors.reset, msg);
-  },
-  warn(msg) {
-    console.log(colors.yellow + "[WARN]" + colors.reset, msg);
-  },
-  error(msg) {
-    console.log(colors.red + "[ERROR]" + colors.reset, msg);
-  }
-};
+function log(type, message) {
+  const logPath = path.join(__dirname, '../../logs', `${type}.log`);
+  const timestamp = new Date().toISOString();
+  fs.appendFileSync(logPath, `[${timestamp}] ${message}\n`);
+}
+
+module.exports = { log };
