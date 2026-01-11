1c45673fc437dbf3404335902b173f70a7864396
aymanjava <kkayman200@gmail.com>
2026-01-11 13:15:38 +0300

    Update fileDB.js

--- src/utils/fileDB.js
+++ src/utils/fileDB.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-function readJSON(file) {
-  const fullPath = path.join(__dirname, '../../database', file);
-  if (!fs.existsSync(fullPath)) return {};
-  return JSON.parse(fs.readFileSync(fullPath, 'utf-8'));
-}
-
-function writeJSON(file, data) {
-  const fullPath = path.join(__dirname, '../../database', file);
-  fs.writeFileSync(fullPath, JSON.stringify(data, null, 2));
-}
-
-module.exports = { readJSON, writeJSON };
+module.exports = {
+    read: (file) => {
+        const filePath = path.join(__dirname, `../database/${file}.json`);
+        if (!fs.existsSync(filePath)) fs.writeFileSync(filePath, '{}');
+        return JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    },
+    write: (file, data) => {
+        const filePath = path.join(__dirname, `../database/${file}.json`);
+        fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+    },
+    append: (file, key, value) => {
+        const db = module.exports.read(file);
+        db[key] = value;
+        module.exports.write(file, db);
+    }
+};
