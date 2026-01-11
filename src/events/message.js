c7b62406b07c294e6ac1ba1a5f76ba38cc44e5fb
aymanjava <kkayman200@gmail.com>
2026-01-11 14:34:36 +0300

    Update message.js

--- src/events/message.js
+++ src/events/message.js
@@ -1,25 +1,9 @@
-const path = require('path');
-const fileDB = require('../utils/fileDB');
-
 module.exports = {
-  name: 'message',
-  execute({ api, event }) {
-    if (!event.body) return;
-    if (event.senderID === api.getCurrentUserID()) return;
+  name: "message",
 
-    const usersPath = path.join(__dirname, '../database/users.json');
-    const threadsPath = path.join(__dirname, '../database/threads.json');
-
-    const users = fileDB.read(usersPath);
-    const threads = fileDB.read(threadsPath);
-
-    if (!users[event.senderID]) users[event.senderID] = { id: event.senderID, messages: 0, firstSeen: Date.now() };
-    if (!threads[event.threadID]) threads[event.threadID] = { id: event.threadID, messages: 0, createdAt: Date.now() };
-
-    users[event.senderID].messages++;
-    threads[event.threadID].messages++;
-
-    fileDB.write(usersPath, users);
-    fileDB.write(threadsPath, threads);
+  run(event, ctx) {
+    ctx.handlers.CommandController.handle(event, ctx);
+    ctx.handlers.AutoReplyController.handle(event, ctx);
+    ctx.handlers.MoodController.handle(event, ctx);
   }
 };
