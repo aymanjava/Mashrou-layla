b29397ec8602ad5e5b73bcc96ca6736194f64a4d
aymanjava <kkayman200@gmail.com>
2026-01-11 13:14:07 +0300

    Update listener.js

--- src/core/listener.js
+++ src/core/listener.js
@@ -1,17 +1,46 @@
-class Listener {
-  constructor(bot) {
-    this.bot = bot;
-  }
+const loader = require('./loader');
+const { EventController } = require('../handlers/EventController');
+const { SpamController } = require('../handlers/SpamController');
+const { AutoReplyController } = require('../handlers/AutoReplyController');
+const { MoodController } = require('../handlers/MoodController');
+const { CommandController } = require('../handlers/CommandController');
+const utils = require('../utils/fileDB');
 
-  listenAll() {
-    this.bot.client.on('message', async (event) => {
-      await this.bot.events.handleEvent('message', event);
-    });
+module.exports = class Listener {
+    constructor(bot) {
+        this.bot = bot;
+        this.commands = loader.loadCommands();
+        this.events = loader.loadEvents();
+        this.features = loader.loadFeatures();
+    }
 
-    this.bot.client.on('event', async (event) => {
-      await this.bot.events.handleEvent(event.type, event);
-    });
-  }
-}
+    async start() {
+        console.log('🟢 Listener is now running...');
 
-module.exports = Listener;
+        this.bot.listen(async (event) => {
+            try {
+                // 1️⃣ فلترة السبام
+                if(await SpamController.isSpam(event.senderID)) return;
+
+                // 2️⃣ تحديد المزاج
+                const mood = MoodController.updateMood(event);
+
+                // 3️⃣ تنفيذ الأوامر
+                if(event.body && event.body.startsWith('!')) {
+                    await CommandController.handle(event, this.commands);
+                }
+
+                // 4️⃣ الردود التلقائية
+                await AutoReplyController.handle(event);
+
+                // 5️⃣ تمرير الحدث للأحداث العامة
+                if(this.events[event.type]) {
+                    await EventController.handle(event, this.events[event.type]);
+                }
+
+            } catch (err) {
+                console.error('Listener Error:', err);
+            }
+        });
+    }
+};
