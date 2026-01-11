12fb4df948df0af60955f851ab9fc7d4bbf84604
aymanjava <kkayman200@gmail.com>
2026-01-11 12:55:04 +0300

    Update fileDB.js

--- src/utils/fileDB.js
+++ src/utils/fileDB.js
@@ -1,20 +1,15 @@
-const fs = require('fs-extra');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = {
-    read: (path) => {
-        try {
-            return fs.readJsonSync(path);
-        } catch (e) {
-            return {};
-        }
-    },
-    write: (path, data) => {
-        try {
-            fs.writeJsonSync(path, data, { spaces: 2 });
-            return true;
-        } catch (e) {
-            console.error("❌ خطأ في حفظ البيانات:", e);
-            return false;
-        }
-    }
-};
+function readJSON(file) {
+  const fullPath = path.join(__dirname, '../../database', file);
+  if (!fs.existsSync(fullPath)) return {};
+  return JSON.parse(fs.readFileSync(fullPath, 'utf-8'));
+}
+
+function writeJSON(file, data) {
+  const fullPath = path.join(__dirname, '../../database', file);
+  fs.writeFileSync(fullPath, JSON.stringify(data, null, 2));
+}
+
+module.exports = { readJSON, writeJSON };
