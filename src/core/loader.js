30479173004a16bcf3d79db2f2de95cfc21eb185
aymanjava <kkayman200@gmail.com>
2026-01-11 13:14:39 +0300

    Update loader.js

--- src/core/loader.js
+++ src/core/loader.js
@@ -1,36 +1,44 @@
 const fs = require('fs');
 const path = require('path');
 
-class Loader {
-  constructor(bot) {
-    this.bot = bot;
-  }
+module.exports = {
+    loadCommands: () => {
+        const commands = {};
+        const commandFolders = fs.readdirSync(path.join(__dirname, '../modules/commands'));
 
-  loadCommands() {
-    const commandsPath = path.join(__dirname, '../modules/commands');
-    const categories = fs.readdirSync(commandsPath);
-    for (const category of categories) {
-      const files = fs.readdirSync(path.join(commandsPath, category));
-      for (const file of files) {
-        const command = require(path.join(commandsPath, category, file));
-        this.bot.commands.register(command);
-      }
+        for (const folder of commandFolders) {
+            const files = fs.readdirSync(path.join(__dirname, `../modules/commands/${folder}`)).filter(f => f.endsWith('.js'));
+            for (const file of files) {
+                const cmd = require(`../modules/commands/${folder}/${file}`);
+                commands[cmd.name] = cmd;
+            }
+        }
+        return commands;
+    },
+
+    loadEvents: () => {
+        const events = {};
+        const files = fs.readdirSync(path.join(__dirname, '../events')).filter(f => f.endsWith('.js'));
+
+        for (const file of files) {
+            const evt = require(`../events/${file}`);
+            events[evt.name] = evt;
+        }
+        return events;
+    },
+
+    loadFeatures: () => {
+        const features = {};
+        const featureFolders = fs.readdirSync(path.join(__dirname, '../modules/features'));
+
+        for (const folder of featureFolders) {
+            features[folder] = {};
+            const files = fs.readdirSync(path.join(__dirname, `../modules/features/${folder}`)).filter(f => f.endsWith('.js'));
+            for (const file of files) {
+                const feature = require(`../modules/features/${folder}/${file}`);
+                features[folder][feature.name] = feature;
+            }
+        }
+        return features;
     }
-  }
-
-  loadEvents() {
-    const eventsPath = path.join(__dirname, '../events');
-    const files = fs.readdirSync(eventsPath);
-    for (const file of files) {
-      const event = require(path.join(eventsPath, file));
-      this.bot.events.register(event);
-    }
-  }
-
-  async loadAll() {
-    this.loadCommands();
-    this.loadEvents();
-  }
-}
-
-module.exports = Loader;
+};
