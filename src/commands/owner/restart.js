cc4ee59128da09e72d8c544633a18a5c4e400b6b
aymanjava <kkayman200@gmail.com>
2026-01-11 00:10:55 +0000

    Delete src/commands directory

--- src/commands/owner/restart.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "restart",
-  description: "🔄 إعادة تشغيل البوت",
-  execute({ api, event }) {
-    if (!global.Layla.utils.permissions.isOwner(event.senderID)) return;
-    api.sendMessage("♻️ جاري إعادة التشغيل…", event.threadID);
-    process.exit(0);
-  }
-};
